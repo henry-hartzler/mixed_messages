09600b3a910b964e8922cb8c5cf6e55da1d7de42
Henry Hartzler <hartzler.henry@gmail.com>
2022-04-05 15:31:14 -0600

    All random indexes working

--- Mixed Messages.js
+++ Mixed Messages.js
@@ -12,3 +12,16 @@ const allPlanets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'U
 const randIndexPlanet = Math.floor(Math.random() * allPlanets.length);
 let randMoon = allPlanets[randIndexPlanet];
 console.log(randMoon);
+
+//Creates a random horoscope
+//Extension idea: break the horoscope into multiple parts: day, prediction, advice
+const horoscope = [
+    'Beware of those who would do you wrong', 
+    'Live life to the fullest, because you never know which day will be your last',
+    'The early bird gets the worm, but the late worm lives another day',
+    'Entrepreneurial opportunities may present themselves when you\'re least expecting it',
+    'Breathe easy, take it all in'
+]
+const randIndexHor = Math.floor(Math.random() * horoscope.length)
+let randHor = horoscope[randIndexHor];
+console.log(randHor);
