97c8fa0e90dc613307cc7be1117987f42a12cf8e
Henry Hartzler <hartzler.henry@gmail.com>
2022-04-05 16:06:48 -0600

    Fully functioning

--- Mixed Messages.js
+++ Mixed Messages.js
@@ -1,27 +1,34 @@
 //This is the initial main branch
 
+const mixedMessage = function() {
+    //Prototype random index
+    const randIndex = arr => {
+        return Math.floor(Math.random() * arr.length);
+}
 //Creating a random astrology sign
-const allAstroSym = ['Ares', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
-const randIndexSym = Math.floor(Math.random() * allAstroSym.length);
-//console.log(randIndexSym);
-let randAstroSym = allAstroSym[randIndexSym];
-console.log(randAstroSym);
+    const allAstroSym = ['Ares', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
+    let randAstroSym = allAstroSym[randIndex(allAstroSym)];
+    //console.log(randAstroSym);
 
 //Chooses a random planet's moon
-const allPlanets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
-const randIndexPlanet = Math.floor(Math.random() * allPlanets.length);
-let randMoon = allPlanets[randIndexPlanet];
-console.log(randMoon);
+    const allPlanets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+    let randMoon = allPlanets[randIndex(allPlanets)];
+    //console.log(randMoon);
+    
 
 //Creates a random horoscope
 //Extension idea: break the horoscope into multiple parts: day, prediction, advice
-const horoscope = [
+    const allHoroscope = [
     'Beware of those who would do you wrong', 
     'Live life to the fullest, because you never know which day will be your last',
     'The early bird gets the worm, but the late worm lives another day',
     'Entrepreneurial opportunities may present themselves when you\'re least expecting it',
-    'Breathe easy, take it all in'
+    'Breathe easy and take it all in'
 ]
-const randIndexHor = Math.floor(Math.random() * horoscope.length)
-let randHor = horoscope[randIndexHor];
-console.log(randHor);
+    let randHor = allHoroscope[randIndex(allHoroscope)];
+    //console.log(randHor);
+
+    return `As a ${randAstroSym}, because of the position of the Sun and ${randMoon}'s moon, your horoscope is: ${randHor}.`;
+}
+
+console.log(mixedMessage());
