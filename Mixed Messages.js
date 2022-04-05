25760aaf671e53e56ffd82f7b931a8575edb6a99
Henry Hartzler <hartzler.henry@gmail.com>
2022-04-05 16:32:21 -0600

    Planets and moons are now randomized but will never be the same.

--- Mixed Messages.js
+++ Mixed Messages.js
@@ -5,17 +5,30 @@ const mixedMessage = function() {
     const randIndex = arr => {
         return Math.floor(Math.random() * arr.length);
 }
+    //Prototype grabs random item from an array
+    const randArr = arr => {
+       return arr[randIndex(arr)]; 
+    }
+
 //Creating a random astrology sign
     const allAstroSym = ['Ares', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
-    let randAstroSym = allAstroSym[randIndex(allAstroSym)];
+    let randAstroSym = randArr(allAstroSym);
     //console.log(randAstroSym);
 
 //Chooses a random planet's moon
     const allPlanets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
-    let randMoon = allPlanets[randIndex(allPlanets)];
+    let randMoon = randArr(allPlanets);
     //console.log(randMoon);
+    const otherPlanet = (arr, moon) => {
+        let other = randArr(arr);
+        if (other !== moon) {
+            return other;
+        } else {
+            return arr[i+1];
+        }
+    }
     
-
+    let otherOne = otherPlanet(allPlanets, randMoon);
 //Creates a random horoscope
 //Extension idea: break the horoscope into multiple parts: day, prediction, advice
     const allHoroscope = [
@@ -25,10 +38,10 @@ const mixedMessage = function() {
     'Entrepreneurial opportunities may present themselves when you\'re least expecting it',
     'Breathe easy and take it all in'
 ]
-    let randHor = allHoroscope[randIndex(allHoroscope)];
+    let randHor = randArr(allHoroscope);
     //console.log(randHor);
 
-    return `As a ${randAstroSym}, because of the position of the Sun and ${randMoon}'s moon, your horoscope is: ${randHor}.`;
+    return `As a ${randAstroSym}, because of the position of the ${otherOne} and ${randMoon}'s moon, your horoscope is: ${randHor}.`;
 }
 
 console.log(mixedMessage());
