ac2c584eeb18e15e3c98e372ad761d2fa84a3821
Henry Hartzler <hartzler.henry@gmail.com>
2022-04-05 15:11:06 -0600

    Random moon

--- Mixed Messages.js
+++ Mixed Messages.js
@@ -2,7 +2,13 @@
 
 //Creating a random astrology sign
 const allAstroSym = ['Ares', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
-const randIndex = Math.floor(Math.random() * allAstroSym.length);
-console.log(randIndex);
-let randAstroSym = allAstroSym[randIndex];
+const randIndexSym = Math.floor(Math.random() * allAstroSym.length);
+//console.log(randIndexSym);
+let randAstroSym = allAstroSym[randIndexSym];
 console.log(randAstroSym);
+
+//Chooses a random planet's moon
+const allPlanets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+const randIndexPlanet = Math.floor(Math.random() * allPlanets.length);
+let randMoon = allPlanets[randIndexPlanet];
+console.log(randMoon);
